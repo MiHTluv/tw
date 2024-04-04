2a543392ebe2a68d55a7d9db93d618427ffffef2
MiHTluv <ngami3003@gmail.com>
2024-04-04 10:36:18 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["public/**/*.{html, js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#cffafe',
+          300: '#67e8f9',
+        }
+      },
+      fontFamily: {
+        body: ['Lato']
+      }
+    },
+  },
+  plugins: [],
+}
